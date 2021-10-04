1d5713e81ea618ffe5eef90554fdfc36766dcde8
AhmedArafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-10-04 20:28:36 +0200

    Update Script.sh

--- Documentation/Script.sh
+++ Documentation/Script.sh
@@ -1,4 +1,4 @@
-// This script is used to save Private Key of container slave in your host {will be used in credential menu in jenkins server} 
+// This script is used to copy Private Key of container slave in your host {will be used in credential menu in jenkins server} 
 // then copy file that containes IPAddress of container slave {will be used for host option while creating new node}
 #! bin/bash
 docker cp $1:/root/.ssh/id_rsa /home/arafat/Desktop/jenkinsproject/PrivateKey
